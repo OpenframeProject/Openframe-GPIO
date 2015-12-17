c66e7dee8e4ba1e6424b765760e4afcc90cd57d3
Jonathan Wohl <jon@jonwohl.com>
2015-12-17 16:42:58 -0500

    changed gpio lib, added test file

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+/**
+ * Openframe plugin which provides a communication channel to the GPIO pins.
+ */
+
 try {
-    var gpio = require('pi-gpio');
+    var gpio = require('rpi-gpio');
 } catch(e) {
     console.error(e);
 }
-/**
- * Example plugin which reads keystrokes from standard in.
- */
 
-module.exports = function(socket, pubsub) {
+module.exports = function(pubsub) {
     'use strict';
     if (!gpio) {
         console.error('\n!!!\nGPIO Not available... is this an RPi?\n!!!\n');
@@ -15,4 +16,16 @@ module.exports = function(socket, pubsub) {
     }
     console.log('loading gpio plugin');
 
+    gpio.on('change', function(channel, value) {
+        console.log('Channel ' + channel + ' value is now ' + value);
+        pubsub.publish('/openframe-gpio/17', value);
+    });
+
+    gpio.setup(17, gpio.DIR_IN, gpio.EDGE_BOTH);
+
+    // gpio.open(17, "input", function(err) {     // Open pin 16 for output
+    //     gpio.read(17, function(val) {          // Set pin 16 high (1)
+    //         pubsub.publish('/openframe-gpio/17', val);
+    //     });
+    // });
 };
