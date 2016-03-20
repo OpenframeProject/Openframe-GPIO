f9cf431ac37de5e8dbd3c64990f69cd7c2acc7e7
Jonathan Wohl <jon@jonwohl.com>
2016-03-20 16:48:18 -0400

    Start updating to latest format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Openframe-GPIO
+
 An example plugin which makes GPIO available to the frame controller.
--- index.js
+++ index.js
@@ -2,25 +2,56 @@
  * Openframe plugin which provides a communication channel to the GPIO pins.
  */
 
+// GPIO not available on all devices...
 try {
     var gpio = require('onoff').Gpio;
 } catch(e) {
     console.error(e);
 }
 
-module.exports = function(pubsub) {
-    'use strict';
+var pjson = require('./package.json'),
+    debug = require('debug')('openframe:gpio'),
+    extension = module.exports = {};
+
+/**
+ * Extension initialization method.
+ *
+ * Called when the extension (and its dependencies) have been installed.
+ *
+ * @param  {object} OF An interface provided to extensions giving limitted access to the frame environment
+ */
+extension.init = function(OF) {
+    // do your extension thing
+    debug('=======>   Openframe-GPIO initialized!   <=======');
+
     if (!gpio) {
         console.error('\n!!!\nGPIO Not available... is this an RPi?\n!!!\n');
         return;
     }
-    console.log('loading gpio plugin');
 
     var button = new gpio(17, 'in', 'both');
 
     button.watch(function(err, state) {
         if (err) console.log(err);
         console.log(state);
-        pubsub.publish('/openframe-gpio/17', state);
+        OF.pubsub.publish('/openframe-gpio/17', state);
     });
+
+    OF.rest().OpenframeUser.OpenframeUser_prototype_primary_collection({
+        id: 'current'
+    }).then(function(data) {
+        console.log(data);
+    });
+
+    /**
+     * Extensions also have access to the global event system
+     */
+    extension.pubsub = OF.getPubsub();
+
+    /**
+     * Extensions also have access to the Swagger REST client (https://github.com/swagger-api/swagger-js)
+     * See openframe.io/explorer for API docs, or openframe.io/explorer/swagger.json for the swagger definition
+     * which shows the available methods as 'operationId'
+     */
+    extension.rest = OF.rest;
 };
