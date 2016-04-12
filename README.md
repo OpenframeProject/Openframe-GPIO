b6f5499221ba1bfb3645fb2e376e03ec92e27db9
Jonathan Wohl <jon@jonwohl.com>
2016-04-12 19:05:20 +0200

    update readme, annotate code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Openframe-GPIO
+# Openframe-GPIOExample
 
-An example plugin which makes GPIO available to the frame controller.
+An example plugin which uses GPIO input from a button to fetch a random artwork from the user's collection.
--- extension.js
+++ extension.js
@@ -46,32 +46,48 @@ extension.init = function(OF) {
         return;
     }
 
+    // using onoff GPIO lib, define pin 17 to be an input
     var button = new gpio(17, 'in', 'both');
 
+    // used to debounce requests... don't make more than one request at a time
     var fetching = false;
 
+    // watch for changes on the button.
     button.watch(function(err, state) {
         if (err) console.log(err);
         console.log(state);
+        // we're only interested in when the button is pressed... if it's pressed
+        // and we're not already fetching, get a random artwork from the collection
         if (state === 1 && !fetching) {
             getRandomFromCollection();
         }
-        // pubsub.publish('/openframe-gpio/17', state);
     });
 
+    /**
+     * Use the REST API to fetch the collection, then select a random artwork to display.
+     * @return {[type]} [description]
+     */
     function getRandomFromCollection() {
         fetching = true;
         // get the logged-in user's primary collection
         rest.OpenframeUser.OpenframeUser_prototype_primary_collection({
             id: 'current'
         }).then(function(data) {
+            // the list of artwork from the collection
             var artworkList = data.obj.collection.artwork,
                 len = artworkList.length,
                 randomIdx = getRandomInt(0, len-1),
                 randomArtwork = artworkList[randomIdx];
 
             debug(randomArtwork);
+
+            // frame.state is the plain JS object representing the frame's state...
+            // set the _current_artwork to be the randomArtwork
             frame.state._current_artwork = randomArtwork;
+
+            // then save the frame to the server. when the db is updated on the server, the server
+            // will trigger a 'frame updated' event, which will in turn update this frame, forcing
+            // the artwork to change to the new one we just set
             frame.save()
                 .then(function() {
                     debug('Success...');
@@ -85,6 +101,7 @@ extension.init = function(OF) {
         });
     }
 
+    // well-named function...
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
